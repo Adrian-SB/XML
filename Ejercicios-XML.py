58c6708736e5a6adde4e7dedafa85a46b9087fe7
Adrián Silva <asboceta@gmail.com>
2017-03-01 11:58:47 +0100

    Ejercicio 2

--- Ejercicios-XML.py
+++ Ejercicios-XML.py
@@ -17,3 +17,20 @@ for n, r in zip(nombre,residencia):
 
     print "Nombre de la empresa:",n.text
     print "Residencia:",r.text,"\n"
+
+#2-Tras introducir una cadena de texto, muestra el número de empresa en las que su descripción coincida con dicha cadena(Tipo de empresa).
+
+palabra=raw_input("Busqueda por palabras claves:")
+
+descripcion=raiz.findall("empresa/descripcion")
+print ""
+
+try:
+    for n, d in zip(nombre,descripcion):
+        contDescripcion=d.text
+        if palabra in contDescripcion:
+            veces=contDescripcion.count(palabra)
+            print "La palabra",palabra,"aparece",veces, "veces en la descripción de",n.text
+except:
+    print ""
+
