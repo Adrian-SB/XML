d8bd8b3c198d22dee2ea8a69e58a41284df5f17a
Adrian-SB <asboceta@gmail.com>
2017-03-08 23:17:11 +0100

    Ejercico 3 y parte del 4

--- Ejercicios-XML.py
+++ Ejercicios-XML.py
@@ -19,7 +19,7 @@ for n, r in zip(nombre,residencia):
     print "Residencia:",r.text,"\n"
 
 #2-Tras introducir una cadena de texto, muestra el número de empresa en las que su descripción coincida con dicha cadena(Tipo de empresa).
-
+"""
 palabra=raw_input("Busqueda por palabras claves:")
 
 descripcion=raiz.findall("empresa/descripcion")
@@ -33,4 +33,25 @@ try:
             print "La palabra",palabra,"aparece",veces, "veces en la descripción de",n.text
 except:
     print ""
+"""
 
+#3-Lista las empresas con más de un teléfono.
+"""
+telefonos=("empresa/telefono")
+
+sep="/"
+
+for n in nombre:
+    if sep in telefonos:
+        print "La asosiacion",n.text, "tiene 2 telefonos"
+"""
+#4-Solicita el nombre de un residencia y muestra la longitud y latitud en un mapa.
+
+longitud=raiz.findall("empresa/longitud")
+latitud=raiz.findall("empresa/latitud")
+
+pal=raw_input("Dime la residencia")
+
+for r,lo,la in zip (residencia,longitud,latitud):
+    if pal == r.text and lo != " " and la != "None":
+        print r.text,"URL ->","\nhttps://www.openstreetmap.org/#map=18/",la.text,"/",lo.text
